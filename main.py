0c7d76a1f0380e9950f611022ad44384e95b3c33
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-08-27 22:18:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def messaging_events(payload):
 def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
-  if "text" == "12345":
+  if text == "12345":
     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
       params={"access_token": token},
       data=json.dumps({
