fbca7625603f35e241308bb634e2e7f4d3105caa
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-08-27 22:04:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ def handle_messages():
   print payload
   for sender, message in messaging_events(payload):
     print "Incoming from %s: %s" % (sender, message)
-    if message == "12345":
-       send_message(PAT, sender, "message")
-    else:
-       send_message(PAT, sender, message)
+    send_message(PAT, sender, message)
   return "ok"
 
 def messaging_events(payload):
