b09352ecb3d37860e64a6e1423a606e13cff0f77
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-08-27 22:08:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def handle_verification():
 def handle_messages():
   print "Handling Messages"
   payload = request.get_data()
+  message2 = "1243124"
   print payload
   for sender, message in messaging_events(payload):
     print "Incoming from %s: %s" % (sender, message)
-    send_message(PAT, sender, message)
+    send_message(PAT, sender, message2)
   return "ok"
 
 def messaging_events(payload):
