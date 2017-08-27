5b953b7c24d1f1ea95cb5f99daf7b0305e297798
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-08-27 20:33:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def send_message(token, recipient, text):
     params={"access_token": token},
     data=json.dumps({
       "recipient": {"id": recipient},
-      "message": {"text": "Привет!"}
+      "message": {"text": "Привет! Я чат-бот этой группы. Чтобы разместить публикацию, отправь мне сообщение со словом POST"}
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
