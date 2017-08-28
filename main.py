29a01e4952ed7d58d7a71bce5b4449d544044691
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-08-28 23:04:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def messaging_events(payload):
 def send_message(token, recipient, text):
   """Send the message text to recipient with id recipient.
   """
-  if re.match('\b\.ru\b'), text):
+  if re.match('\b\.ru\b', text):
     r = requests.post("https://graph.facebook.com/v2.6/me/messages",
       params={"access_token": token},
       data=json.dumps({
